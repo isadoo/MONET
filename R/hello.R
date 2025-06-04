fec357faf4e0a7574c4d8b2e959f5f5859f1bc14
Isabela <isabela.doo@unil.ch>
2025-06-04 22:54:42 +0200

    add license and export tag to test function

--- R/hello.R
+++ R/hello.R
@@ -2,6 +2,7 @@
 #' @title Hello World Function
 #' @description A simple function that prints "Hello, world!" to the console.
 #' @return NULL
+#' @export
 #' @examples
 #' hello()
 hello <- function() {
