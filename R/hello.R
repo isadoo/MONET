a945adfe3e17a3bea1c5b21b3725dcf722bd11c6
isadoo <isabela.doo@unil.ch>
2025-06-04 21:23:33 +0200

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+###########################################################################
+#' @title Hello World Function
+#' @description A simple function that prints "Hello, world!" to the console.
+#' @return NULL
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
