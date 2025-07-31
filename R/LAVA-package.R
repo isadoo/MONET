d8f83cb2444d6da303d039afdf5d2bd27750d21e
Isa do O <isabela.doo@unil.ch>
2025-07-31 11:58:58 +0200

    Refactor coancestries_calculate function to calculate_coancestries; update imports, documentation, and tests accordingly

--- R/LAVA-package.R
+++ R/LAVA-package.R
@@ -7,6 +7,9 @@
 #' @importFrom stats density gaussian median quantile var
 #' @importFrom brms brm VarCorr hypothesis as_draws_df
 #' @importFrom hierfstat mat2vec
+#' @importFrom dplyr select distinct count rename full_join mutate
+#' @importFrom tidyr replace_na
+#' @importFrom magrittr %>%
 ## usethis namespace: end
 NULL
 
@@ -20,7 +23,7 @@ NULL
 #' The LAVA package provides several key functions:
 #' \itemize{
 #'   \item \code{\link{lava}}: Main function for estimating log ratios of ancestral variances
-#'   \item \code{\link{coancestries_calculate}}: Calculate coancestry matrices from genetic data
+#'   \item \code{\link{calculate_coancestries}}: Calculate coancestry matrices from genetic data
 #'   \item \code{\link{kinship_from_pedigree}}: Calculate kinship matrix from pedigree data
 #'   \item \code{\link{counting_blocks_matrix}}: Utility function for matrix block counting
 #' }
